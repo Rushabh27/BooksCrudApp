f0572e83c9b2ab15d255314deec352065b3ad097
Rushabh27 <rushabh.dholakia05@gmail.com>
2023-11-07 00:20:31 +0530

    added comments

--- api/api.js
+++ api/api.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const bookModel = require('../model/book_model');
 
+// route for displaying all books data
 router.get('/books', async function (req, res) {
     const bookList = await bookModel.book.find();
     console.log(bookList);
     res.send(bookList);
 });
 
+// route for displayng specific book based on id passed as params
 router.get('/books/:id', async function (req, res) {
     const { id } = req.params;
 
@@ -16,16 +18,16 @@ router.get('/books/:id', async function (req, res) {
     res.send(book);
 });
 
+// route for inserting new book data
 router.post('/books', async function (req, res) {
     const Title = req.body.title;
     const Summary = req.body.summary;
     const Author = req.body.author;
     const bookExist = await bookModel.book.find();
     console.log(bookExist);
-    // console.log(bookExist[0].Book_id);
     let id = 0;
 
-    if (bookExist && bookExist.length > 0) id = bookExist[0].Book_id + 1;
+    if (bookExist && bookExist.length > 0) id = bookExist[0].Book_id + 1; // increment the id by 1 and store for each new book data
 
     var data = await bookModel.book.create({ Title, Author, Summary, Book_id: id });
     data.save();
@@ -33,7 +35,7 @@ router.post('/books', async function (req, res) {
     res.send("Book Uploaded");
 });
 
-
+// route for updating book records by particular id sent as params
 router.put('/books/:id', async function (req, res) {
     const { id } = req.params;
     const {
@@ -61,11 +63,12 @@ router.put('/books/:id', async function (req, res) {
     res.status(200).send("Book Updated");
 });
 
+// route for deleting the book record specified by the id
 router.delete('/books/:id', async function (req, res) {
     const { id } = req.params;
 
     const bookExist = await bookModel.book.findOne({ Book_id: id });
-    if (!bookExist) return res.send('Book Do Not exist');
+    if (!bookExist) return res.send('Book Does Not exist');
 
     await bookModel.book.deleteOne({ Book_id: id }).then(function () {
         console.log("Data deleted"); // Success
